30df79c0e416c15e53cb4d0ea26b203496fbaccc
nikikumar <niki.kumar@gooru.org>
2023-05-02 15:47:00 +0530

    return third lowest and third highest in the array without using index

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-//post api
-const express = require ("express")
-const app = express();
-
-app.post('/',(req,res)=>{
-    res.send("received post request");
-    res.end()
-})
-
-app.listen(3000,()=>{
-    console.log("port is running at",3000)
-})
+//return third lowest and third highest in the array without using the index
+let arr=[1,4,7,2,5,6,3,22,11,10];
+console.log(arr.sort((a,b)=>a-b)); //mutates the array
+console.log(arr);
+console.log(arr.slice(2,3));
+console.log(arr.slice(arr.length-3,arr.length-2));
 
 
