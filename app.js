3457e54b6b952899d734c1d670091fc8f27b7e96
nikikumar <niki.kumar@gooru.org>
2023-05-02 16:46:12 +0530

    synchronous/asynchronous operation in node

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-//return third lowest and third highest in the array without using the index
-let arr=[1,4,7,2,5,6,3,22,11,10];
-console.log(arr.sort((a,b)=>a-b)); //mutates the array
-console.log(arr);
-console.log(arr.slice(2,3));
-console.log(arr.slice(arr.length-3,arr.length-2));
+const fs = require('fs');
 
+const data = fs.readFileSync("readme.md","utf-8");
+console.log(data,"read sync");
+console.log("after data");
 
+const data2 = fs.readFile("readme.md","utf-8",(err,data)=>{
+    console.log(data,"async data")
+})
+
+console.log("before async data")
